ebe1df5a0e9c307ce68fed59cde67d3eb59b4d7c
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2026-06-12 15:12:12 -0300

    estudando  sobre tupla

---
+++ tupla/tupla-1.py
@@ -0,0 +1,13 @@
+tupla = (10, 20, 30)
+print(f"Tipo de tupla: {type(tupla)}")
+
+print("Tupla toda")
+print(tupla)
+
+print("Primeiro elemento")
+print(tupla[0])
+
+print("Terceiro elemento")
+print(tupla[2])
+
+tupla[1] = 2000
