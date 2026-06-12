15c971df7596cca27798d06d0a9a3d2e82561def
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2026-06-12 15:22:59 -0300

    estudando conjuntos

---
+++ conjuntos/conjunto-1.py
@@ -0,0 +1,7 @@
+# Conjuntos: elementos não ordenados (sem posição) e não podem se repetir.
+# representados por chaves "{}"
+
+conjunto = {10, 8, 2, 3, 8, 10}
+print(f"Tipo do conjunto: {type(conjunto)}")
+
+print(conjunto)
---
+++ conjuntos/conjunto-2.py
@@ -0,0 +1,7 @@
+a = {1, 2, 3}
+b = {3, 2, 4, 5}
+
+print(a | b) # Operação de união.
+print(a & b) # Operação de intercessãoo.
+print(a - b) # Operação de diferença.
+
