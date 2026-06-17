8b81dd52ffa3fc8a1a7e6b4feda284a5732577ad
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2026-06-17 15:21:52 -0300

    Exercício de fixação sobre dicionário

---
+++ Exercício-dicionário.py
@@ -0,0 +1,11 @@
+# Crie um dicionário que armazene as informações de um carro, informações essas que serão a marca, modelo e ano. 
+# Em seguida, exiba uma frase apresentando essas informações do carro, no seguinte formato: 
+# O carro e um MARCA MODELO do ano ANO.
+
+carro = {
+    "marca": "toyota", 
+    "modelo": "yaris",
+    "ano": 2025
+}
+
+print(f"O carro é um {carro['marca']} {carro['modelo']} do ano {carro['ano']}")
